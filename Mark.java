e7dbf045efe1f41daef3cd0ef464041c6d27a847
khesten <florian028@gmx.com>
2017-05-16 12:37:30 +0200

    Add files via upload

---
+++ Mark.java
@@ -0,0 +1,17 @@
+
+public class Mark implements PlayingPiece{
+	
+	private int[] coordinates;
+	private String type;
+	public Mark(int[] coordinates, String type){
+		this.coordinates=coordinates;
+		this.type=type;
+	}
+	
+	public int[] getCoordinates(){
+		return coordinates;
+	}
+	public String getType(){
+		return type;
+	}
+}
---
+++ PlayingPiece.java
@@ -0,0 +1,7 @@
+
+public interface PlayingPiece {
+	public int[] getCoordinates();
+	public String getType();
+	
+
+}

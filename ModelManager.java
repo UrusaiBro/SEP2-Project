8b8c6285a32ac0d625905352a70a0b703719e91e
UrusaiBro <Urusai.bro@gmail.com>
2017-05-18 09:26:03 +0200

    Update ModelManager.java

--- ModelManager.java
+++ ModelManager.java
@@ -1,14 +1,22 @@
 
-public class ModelManager implements Model {
+public class ModelManager implements Model 
+{
 	private Board board;
 	
-	public ModelManager(Board board){
-		this.board=board;
+	public ModelManager()
+	{
+		board = new TicTacToe();
 	}
 	
-	public PlayingPiece interact(int[] coordinates){
+	public void createGame()
+	{
+		board = new TicTacToe();
+	}
+	
+	public PlayingPiece interact(int[] coordinates)
+	{
+		
 		return board.interact(coordinates);
 	}
-	
 
 }

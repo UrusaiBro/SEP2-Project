cf1a00098a7ae60ccfb30525c4b29465d145d876
UrusaiBro <Urusai.bro@gmail.com>
2017-05-18 09:26:55 +0200

    Update TicTacToe.java

--- TicTacToe.java
+++ TicTacToe.java
@@ -1,9 +1,11 @@
 public class TicTacToe implements Board {
 
 	private PlayingPiece[][] board;
-
+	private boolean X;
+	
 	public TicTacToe() {
 		board = new PlayingPiece[3][3];
+		X = true;
 	}
 
 	@Override
@@ -13,14 +15,15 @@ public class TicTacToe implements Board {
 
 	@Override
 	public PlayingPiece interact(int[] coordinates) {
-		boolean X = true;
+		
 		if (X) {
 			board[coordinates[0]][coordinates[1]] = new Mark(new int[] { coordinates[0], coordinates[1] }, "X");
+			System.out.println(coordinates[0] + "test" + coordinates[1]);
 			X = false;
 		}
-
 		else {
 			board[coordinates[0]][coordinates[1]] = new Mark(new int[] { coordinates[0], coordinates[1] }, "O");
+			System.out.println(coordinates[0] + "test" + coordinates[1]);
 			X = true;
 		}
 
